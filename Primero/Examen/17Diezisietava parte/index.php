6af4cfadf7268d58a02a638944cd468e5680bfaf
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2025-05-09 18:42:23 +0200

    parte 17 t3

---
+++ Primero/Examen/17Diezisietava parte/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Portafolio</title>
+        <link rel="Stylesheet" href="general.css">
+        <link rel="Stylesheet" href="footer.css">
+        <link rel="Stylesheet" href="header.css">
+        <body>
+            <header>
+                <h1>Portafolio de Andrei Buga</h1>
+                <p>En este portafolio se expondran todas las aplicaciones que he realizado</p>
+            </header>
+            <?php include "main/main.php";?>
+            <footer>
+                <p>© 2025 Andrei Buga. Todos los derechos reservados.</p>
+            </footer>
+        </body>
+    </head>
+</html>
